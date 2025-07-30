c374375543e26ba73422ba5345a9d24187fab51d
Mitchell Barton <mitchellcliftonbarton@gmail.com>
2025-07-30 14:04:00 -0600

    seo cloudflare

--- svelte/src/routes/api/subscribe-user/+server.js
+++ svelte/src/routes/api/subscribe-user/+server.js
@@ -1,11 +1,7 @@
 import { json } from '@sveltejs/kit';
 import mailchimp from '@mailchimp/mailchimp_marketing';
 import md5 from 'md5';
-import {
-	MAILCHIMP_API_KEY,
-	MAILCHIMP_AUDIENCE_ID,
-	MAILCHIMP_API_SERVER
-} from '$env/static/private';
+import { env } from '$env/dynamic/private';
 
 export async function POST({ request }) {
 	// get email from request
@@ -16,12 +12,12 @@ export async function POST({ request }) {
 
 	// set mailchimp config
 	mailchimp.setConfig({
-		apiKey: MAILCHIMP_API_KEY,
-		server: MAILCHIMP_API_SERVER
+		apiKey: env.MAILCHIMP_API_KEY,
+		server: env.MAILCHIMP_API_SERVER
 	});
 
 	// make request to mailchimp
-	const mcRequest = await mailchimp.lists.setListMember(MAILCHIMP_AUDIENCE_ID, emailHash, {
+	const mcRequest = await mailchimp.lists.setListMember(env.MAILCHIMP_AUDIENCE_ID, emailHash, {
 		email_address: email,
 		status_if_new: 'pending', // we need to set the list to allow double opt in
 		merge_fields: {
